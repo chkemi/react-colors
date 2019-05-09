15b4d1dc415dd183012490c2f2623d88a8d9b38e
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-05-09 08:44:48 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>Palette list goes here</h1>} />
+      <Route 
+        exact 
+        path='/palette/:id' 
+        render={() => <h1>Individual palette</h1>}
+        />
+    </Switch>
+
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
