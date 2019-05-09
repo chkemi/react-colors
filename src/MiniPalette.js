10ddf1e24d95cc3f718e6d8252232ca6b04e8c6b
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-05-09 09:34:36 +0200

    started styling palletelist

---
+++ src/MiniPalette.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+    root: {
+        background: 'white',
+        border: '1px solid black',
+        borderRadius: '5px',
+        padding: '0.5rem',
+        position: 'relative',
+        overflow: 'hidden',
+        "&:hover": {
+            cursor: 'pointer'
+        }
+    },
+    colors: {
+        backgroundColor: 'grey'
+    },
+    title: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '0',
+        color: 'black',
+        paddingTop: '0.5rem',
+        fontSize: '1rem',
+        position: 'relative'
+    },
+};
+
+const MiniPalette = (props) => {
+    const { classes, paletteName } = props;
+
+    return ( 
+        <div className={classes.root}>
+            <div className={classes.colors}></div>
+            <h5 className={classes.title}>{paletteName}</h5>
+        </div>
+    );
+}
+ 
+export default withStyles(styles)(MiniPalette);
