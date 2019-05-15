15fe1cf563e0544c4857808adfd4fae24ea75fc7
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-05-15 13:32:53 +0200

    animated palette deletion

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,6 +3,10 @@ import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/PaletteListStyles';
 import { Link } from 'react-router-dom';
+import {
+    CSSTransition,
+    TransitionGroup,
+  } from 'react-transition-group';
 
 class PaletteList extends Component {
     constructor(props) {
@@ -24,17 +28,23 @@ class PaletteList extends Component {
                         <h1 className={classes.heading}>React Colors</h1>
                         <Link to='/palette/new'>Create Palette</Link>
                     </nav>
-                    <div className={classes.palettes}>
-                        {palettes.map((palette) => (
-                            <MiniPalette  
-                                {...palette} 
-                                handleClick={() => this.goToPalette(palette.id)} 
-                                handleDelete={deletePalette}
-                                key={palette.id}
-                                id={palette.id}
-                            />
-                        ))}
-                    </div>
+                        <TransitionGroup className={classes.palettes}>
+                            {palettes.map((palette) => (
+                                <CSSTransition 
+                                    key={palette.id} 
+                                    classNames='fade'
+                                    timeout={500}
+                                >
+                                    <MiniPalette  
+                                        {...palette} 
+                                        handleClick={() => this.goToPalette(palette.id)} 
+                                        handleDelete={deletePalette}
+                                        key={palette.id}
+                                        id={palette.id}
+                                    />
+                                </CSSTransition>
+                            ))}
+                        </TransitionGroup>
                 </div>
             </div>
          );
